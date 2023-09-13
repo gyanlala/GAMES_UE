576cecc056e97d25e9c9d37a00c4edc16390c3bf
qin_chu_yan <806334626@qq.com>
2023-09-13 14:30:58 +0800

    作业1

---
+++ hw1/Source/fps/fpsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "fpsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AfpsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AfpsGameMode();
+};
+
+
+
